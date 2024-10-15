ea584fe646a93ac169876d1c529b47a1fb9dccc4
[CLOUD4] 심우민 <worejj@naver.com>
2024-10-15 10:42:02 +0900

    feat #9 상품 컴포넌트 구현

---
+++ src/components/Product/ProductIem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TableRow, TableCell, IconButton } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const ProductItem = ({ product, onEdit, onDelete }) => {
+  const formattedProductId = String(product.productId).padStart(4, '0');
+
+  return (
+    <TableRow>
+      <TableCell>{formattedProductId}</TableCell>
+      <TableCell>{product.name}</TableCell>
+      <TableCell>{product.description}</TableCell>
+      <TableCell>
+          {product.isDeleted ? (<span style={{ color: 'red' }}>삭제됨</span>) : 
+          (<span style={{ color: 'green' }}>활성</span>
+  )}
+</TableCell>
+      <TableCell>
+        <IconButton onClick={() => onEdit(product)} aria-label="edit">
+          <EditIcon />
+        </IconButton>
+        <IconButton onClick={() => onDelete(product)} aria-label="delete">
+          <DeleteIcon />
+        </IconButton>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default ProductItem;
---
+++ src/components/Product/ProductList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import ProductItem from './ProductIem';
+
+const ProductList = ({ products, onEdit, onDelete }) => {
+
+    
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>분류 코드</TableCell>
+          <TableCell>상품명</TableCell>
+          <TableCell>상품 설명</TableCell>
+          <TableCell>삭제 여부</TableCell>
+          <TableCell>관리</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {products.map((product) => (
+          <ProductItem
+            key={product.productId}
+            product={product}
+            onEdit={onEdit}
+            onDelete={onDelete}
+          />
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+
+export default ProductList;
