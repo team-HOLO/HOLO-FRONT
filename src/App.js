d489bfa84f06630e3473150fe7a195deb59b0a37
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-10-20 01:23:27 +0900

    feat #23 회원정보 수정페이지

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ import AdminDashboard from "./pages/admin/AdminDashboard";
 import Main from "./pages/Main";
 import ProductManagementPage from "./pages/admin/ProductManagementPage";
 import axios from "axios";
-import ProductList from './components/Product/productList/ProductList';
+import ProductList from "./components/Product/productList/ProductList";
 import ProductDetails from "./components/Product/ProductDetails";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
+import MemberUpdatePage from "./pages/SignUpPage";
 
 const theme = createTheme({
   palette: {
@@ -80,6 +81,7 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/signin" element={<SignInPage />} />
               <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/memberupdate" element={<MemberUpdatePage />} />
             </Routes>
           </div>
           <Footer />
---
+++ src/pages/MemberUpdate.js
@@ -0,0 +1,10 @@
+// SignInPage.js
+import React from "react";
+
+import MemberUpdate from "../components/member/sign-up/MemberUpdate";
+
+const MemberUpdatePage = () => {
+  return <MemberUpdate />;
+};
+
+export default MemberUpdatePage;
