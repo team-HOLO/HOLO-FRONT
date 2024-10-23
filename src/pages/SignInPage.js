0e62a6fe155a15605a965f83e28f7c6920c083b1
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-10-24 02:45:58 +0900

    feat #53  - 회원 비밀번호 확인 구현

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,9 +1,16 @@
-// SignInPage.js
 import React from "react";
 import SignIn from "../components/member/signin/SignIn";
+import { CssBaseline, GlobalStyles } from "@mui/material"; // GlobalStyles 가져오기
 
 const SignInPage = () => {
-  return <SignIn />;
+  return (
+    <>
+      <CssBaseline enableColorScheme />
+      {/* GlobalStyles로 전역 스타일 추가 */}
+      <GlobalStyles styles={{ "*": { boxSizing: "border-box" } }} />
+      <SignIn />
+    </>
+  );
 };
 
 export default SignInPage;
