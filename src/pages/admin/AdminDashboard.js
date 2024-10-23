32bd5276c5c37b30ad0a54ba964cf7881ae9d56e
[CLOUD4] 백승주 <bsj5967@naver.com>
2024-10-23 10:55:37 +0900

    feat #42 회원 관리 제거, 쇼핑카트 아이콘 수정

--- src/pages/admin/AdminDashboard.js
+++ src/pages/admin/AdminDashboard.js
@@ -1,67 +1,76 @@
-import React from 'react';
-import {Box, Grid, Paper} from '@mui/material';
+import React from "react";
+import { Box, Grid, Paper } from "@mui/material";
 import AdminMenu from "components/admin/AdminMenu";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
-    const menuItems = [
-        { image: 'images/members.svg', title: '회원관리', desc: '회원 조회, 수정, 삭제', link: 'members' },
-        { image: 'images/orders.svg', title: '주문 관리', desc: '주문 조회, 상태 변경, 취소', link: 'orders'},
-        { image: 'images/products.svg', title: '상품 관리', desc: '상품 조회, 추가, 수정, 삭제', link: 'products'},
-        { image: '/images/category.svg', title: '카테고리 관리', desc: '카테고리 조회, 추가, 수정, 삭제', link: 'categories'},
-    ];
+  const menuItems = [
+    {
+      image: "images/orders.svg",
+      title: "주문 관리",
+      desc: "주문 조회, 상태 변경, 취소",
+      link: "orders",
+    },
+    {
+      image: "images/products.svg",
+      title: "상품 관리",
+      desc: "상품 조회, 추가, 수정, 삭제",
+      link: "products",
+    },
+    {
+      image: "/images/category.svg",
+      title: "카테고리 관리",
+      desc: "카테고리 조회, 추가, 수정, 삭제",
+      link: "categories",
+    },
+  ];
 
-    return (
-        <Box
-            sx={{
-                display: 'static',
-                alignItems: 'center',
-                justifyContent: 'center',
-                minHeight: '100vh', // 화면의 높이만큼 꽉 채움
-                padding: '2rem'
-            }}
-        >
-            <Grid container spacing={4} justifyContent="center" alignItems="center">
-                {menuItems.map((item, index) => (
-                    <Grid
-                        item
-                        xs={12}
-                        sm={6}
-                        md={6}
-                        lg={3}
-                        xl={2}
-                        key={index}
-                    >
-                        <Link to={`/admin/${item.link}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <Paper
-                                elevation={3}
-                                sx={{
-                                    borderRadius: '8px',
-                                    textAlign: 'center',
-                                    transition: 'transform 0.3s',
-                                    '&:hover': {
-                                        transform: 'scale(1.05)'
-                                    }
-                                }}
-                            >
-                                <AdminMenu
-                                    image={item.image}
-                                    title={item.title}
-                                    desc={item.desc}
-                                    sx={{
-                                        width: '80%',
-                                        height: 'auto',
-                                        maxWidth: '250px',
-                                        margin: '0 auto',
-                                    }}
-                                />
-                            </Paper>
-                        </Link>
-                    </Grid>
-                ))}
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        display: "static",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh", // 화면의 높이만큼 꽉 채움
+        padding: "2rem",
+      }}
+    >
+      <Grid container spacing={4} justifyContent="center" alignItems="center">
+        {menuItems.map((item, index) => (
+          <Grid item xs={12} sm={6} md={6} lg={3} xl={2} key={index}>
+            <Link
+              to={`/admin/${item.link}`}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              <Paper
+                elevation={3}
+                sx={{
+                  borderRadius: "8px",
+                  textAlign: "center",
+                  transition: "transform 0.3s",
+                  "&:hover": {
+                    transform: "scale(1.05)",
+                  },
+                }}
+              >
+                <AdminMenu
+                  image={item.image}
+                  title={item.title}
+                  desc={item.desc}
+                  sx={{
+                    width: "80%",
+                    height: "auto",
+                    maxWidth: "250px",
+                    margin: "0 auto",
+                  }}
+                />
+              </Paper>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default AdminDashboard;
--- src/pages/admin/AdminPage.js
+++ src/pages/admin/AdminPage.js
@@ -1,54 +1,58 @@
-import React from 'react';
-import { Outlet, Link, useLocation } from 'react-router-dom';
-import { Box, List, ListItem, ListItemText, ListItemButton, Toolbar, Drawer } from '@mui/material';
+import React from "react";
+import { Outlet, Link, useLocation } from "react-router-dom";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  Toolbar,
+  Drawer,
+} from "@mui/material";
 
 const AdminPage = () => {
-    const location = useLocation();
-    const isRootAdminPath = location.pathname === '/admin';
+  const location = useLocation();
+  const isRootAdminPath = location.pathname === "/admin";
 
-    return (
-        <Box display="flex" height="100vh">
-            {!isRootAdminPath && (
-                <Drawer
-                    variant="permanent"
-                    sx={{
-                        width: 240,
-                        flexShrink: 0,
-                        [`& .MuiDrawer-paper`]: { width: 240, boxSizing: 'border-box' },
-                    }}
-                >
-                    <Toolbar />
-                    <Box sx={{ overflow: 'auto' }}>
-                        <List>
-                            <ListItem disablePadding>
-                                <ListItemButton component={Link} to="/admin/orders">
-                                    <ListItemText primary="주문 관리" />
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem disablePadding>
-                                <ListItemButton component={Link} to="/admin/products">
-                                    <ListItemText primary="상품 관리" />
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem disablePadding>
-                                <ListItemButton component={Link} to="/admin/members">
-                                    <ListItemText primary="회원 관리" />
-                                </ListItemButton>
-                            </ListItem>
-                            <ListItem disablePadding>
-                                <ListItemButton component={Link} to="/admin/categories">
-                                    <ListItemText primary="카테고리 관리" />
-                                </ListItemButton>
-                            </ListItem>
-                        </List>
-                    </Box>
-                </Drawer>
-            )}
-            <Box flexGrow={1} p={3} bgcolor="white">
-                <Outlet />
-            </Box>
-        </Box>
-    );
+  return (
+    <Box display="flex" height="100vh">
+      {!isRootAdminPath && (
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: 240,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: { width: 240, boxSizing: "border-box" },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: "auto" }}>
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton component={Link} to="/admin/orders">
+                  <ListItemText primary="주문 관리" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton component={Link} to="/admin/products">
+                  <ListItemText primary="상품 관리" />
+                </ListItemButton>
+              </ListItem>
+
+              <ListItem disablePadding>
+                <ListItemButton component={Link} to="/admin/categories">
+                  <ListItemText primary="카테고리 관리" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
+      )}
+      <Box flexGrow={1} p={3} bgcolor="white">
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default AdminPage;
